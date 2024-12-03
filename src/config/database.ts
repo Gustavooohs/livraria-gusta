288522595798136593a4938bd31270324b72a744
Seu Nome <seu-email@exemplo.com>
2024-12-02 22:21:55 -0300

    dependencia cors para validação do usuário

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+import { Pool } from 'pg';
+
+const connectionString = 'postgresql://backendfinal_user:7YoHcHI7dCCzrgVpocXb8Pm3DFOclgJk@dpg-ct74mkd2ng1s73c9vq70-a.oregon-postgres.render.com/backendfinal';
+
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  }
+});
+
+export default pool;
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import authRoutes from './routes/authRoutes';
+import userRoutes from './routes/userRoutes';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(userRoutes);
+app.use(authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
